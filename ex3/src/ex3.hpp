992577b387f5f7a09b60b4754a48fb0441417d38
sharim <panyi6034@gmail.com>
2021-05-11 16:56:16 +0800

    task1 completed

---
+++ ex3/src/ex3.hpp
@@ -0,0 +1,96 @@
+#ifndef EX3_HPP
+#define EX3_HPP
+
+#include <opencv2/opencv.hpp>
+
+namespace ex3 {
+void circular_shift(cv::InputArray src, cv::OutputArray dst, cv::Point delta);
+void fft_shift(cv::InputArray src, cv::OutputArray dst);
+void ifft_shift(cv::InputArray src, cv::OutputArray dst);
+void FT(cv::InputArray src, cv::OutputArray dst);
+void freq(cv::InputArray src, cv::OutputArray dst, bool shift = false);
+
+void circular_shift(cv::InputArray src, cv::OutputArray dst, cv::Point delta) {
+  cv::Mat _src = src.getMat();
+  dst.create(_src.size(), _src.type());
+  cv::Mat _dst = dst.getMat();
+
+  int delta_x = delta.x % (_src.size().width - 1);
+  int delta_y = delta.y % (_src.size().height - 1);
+
+  int idx_x = delta_x >= 0 ? delta_x : _src.size().width + delta_x;
+  int idx_y = delta_y >= 0 ? delta_y : _src.size().height + delta_y;
+
+  cv::Mat tmp(_src.size(), _src.type());
+
+  _src(cv::Rect(0, 0, _dst.size().width - idx_x, _dst.size().height))
+      .copyTo(tmp(
+          cv::Rect(idx_x, 0, _dst.size().width - idx_x, _dst.size().height)));
+
+  _src(cv::Rect(_dst.size().width - idx_x, 0, idx_x, _dst.size().height))
+      .copyTo(tmp(cv::Rect(0, 0, idx_x, _dst.size().height)));
+
+  tmp(cv::Rect(0, 0, _dst.size().width, _dst.size().height - idx_y))
+      .copyTo(_dst(
+          cv::Rect(0, idx_y, _dst.size().width, _dst.size().height - idx_y)));
+
+  tmp(cv::Rect(0, _dst.size().height - idx_y, _dst.size().width, idx_y))
+      .copyTo(_dst(cv::Rect(0, 0, _dst.size().width, idx_y)));
+}
+
+void fft_shift(cv::InputArray src, cv::OutputArray dst) {
+  cv::Mat _src = src.getMat();
+  cv::Point delta;
+  delta.x = _src.size().width / 2;
+  delta.y = _src.size().height / 2;
+  circular_shift(src, dst, delta);
+}
+
+void ifft_shift(cv::InputArray src, cv::OutputArray dst) {
+  cv::Mat _src = src.getMat();
+  cv::Point delta;
+  delta.x = -_src.size().width / 2;
+  delta.y = -_src.size().height / 2;
+  circular_shift(src, dst, delta);
+}
+
+void FT(cv::InputArray src, cv::OutputArray dst) {
+  cv::Mat _src = src.getMat();
+
+  int M = cv::getOptimalDFTSize(_src.rows);
+  int N = cv::getOptimalDFTSize(_src.cols);
+  cv::Mat padded;
+  cv::copyMakeBorder(_src, padded, 0, M - _src.rows, 0, N - _src.cols,
+                     cv::BORDER_CONSTANT, cv::Scalar::all(0));
+  cv::Mat planes[] = {cv::Mat_<float>(padded),
+                      cv::Mat::zeros(padded.size(), CV_32F)};
+  cv::Mat complexImg;
+  cv::merge(planes, 2, complexImg);
+  cv::dft(complexImg, complexImg);
+  cv::split(complexImg, planes);
+  cv::magnitude(planes[0], planes[1], planes[0]);
+  cv::Mat mag = planes[0];
+  mag += cv::Scalar::all(1);
+  cv::log(mag, mag);
+
+  dst.create(cv::Size(mag.cols & -2, mag.rows & -2), CV_32FC1);
+  cv::Mat _dst = dst.getMat();
+  mag(cv::Rect(0, 0, mag.cols & -2, mag.rows & -2)).copyTo(_dst);
+}
+
+void freq(cv::InputArray src, cv::OutputArray dst, bool shift) {
+  cv::Mat _src = src.getMat();
+  CV_Assert(_src.type() == CV_8UC1);
+  cv::Mat tmp;
+  FT(_src, tmp);
+  if (shift)
+    fft_shift(tmp, tmp);
+  cv::normalize(tmp, tmp, 0, 255, cv::NORM_MINMAX);
+  dst.create(tmp.size(), CV_8UC1);
+  cv::Mat _dst = dst.getMat();
+  tmp.convertTo(_dst, CV_8UC1);
+}
+
+} // namespace ex3
+
+#endif /* EX3_HPP */
---
+++ ex3/src/main.cpp
@@ -0,0 +1,64 @@
+#include "ex3.hpp"
+#include <opencv2/opencv.hpp>
+
+// #define SAVE_IMG
+// #define TASK_1
+#define TASK_2
+// #define TASK_3
+// #define TASK_4
+
+int main(int argc, char **argv) {
+
+  /* ---------- Read images ---------- */
+  cv::Mat img_cave =
+      cv::imread("../img/src_img/cave.jpg", cv::IMREAD_GRAYSCALE);
+  cv::Mat img_lena =
+      cv::imread("../img/src_img/lena.bmp", cv::IMREAD_GRAYSCALE);
+
+  /* ---------- Task 1 ---------- */
+#ifdef TASK_1
+  cv::Mat img_rect =
+      cv::imread("../img/src_img/rect.bmp", cv::IMREAD_GRAYSCALE);
+  cv::Mat img_rect_45 =
+      cv::imread("../img/src_img/rect-45度.bmp", cv::IMREAD_GRAYSCALE);
+  cv::Mat img_rect_move =
+      cv::imread("../img/src_img/rect-move.bmp", cv::IMREAD_GRAYSCALE);
+  cv::Mat img_rect_dft, img_rect_45_dft, img_rect_move_dft;
+
+  ex3::freq(img_rect, img_rect_dft, true);
+  ex3::freq(img_rect_45, img_rect_45_dft, true);
+  ex3::freq(img_rect_move, img_rect_move_dft, true);
+
+  cv::imshow("rect", img_rect_dft);
+  cv::imshow("rect45", img_rect_45_dft);
+  cv::imshow("rctmove", img_rect_move_dft);
+  cv::waitKey(0);
+
+#ifdef SAVE_IMG
+  cv::imwrite("../img/gen_img/rect_dft.jpg", img_rect_dft);
+  cv::imwrite("../img/gen_img/rect_45_dft.jpg", img_rect_45_dft);
+  cv::imwrite("../img/gen_img/img_rect_move.jpg", img_rect_move_dft);
+#endif
+#endif
+
+  /* ---------- Task 2 ---------- */
+#ifdef TASK_2
+  cv::Mat img_grid =
+      cv::imread("../img/src_img/grid.bmp", cv::IMREAD_GRAYSCALE);
+  cv::Mat img_grid_dft;
+
+  ex3::freq(img_grid, img_grid_dft, true);
+  cv::imshow("grid_dft", img_grid_dft);
+  cv::waitKey(0);
+#endif
+
+  /* ---------- Task 3 ---------- */
+#ifdef TASK_3
+#endif
+
+  /* ---------- Task 4 ---------- */
+#ifdef TASK_4
+#endif
+
+  return 0;
+}
